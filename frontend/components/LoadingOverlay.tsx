bbb6344786dea643879cfb985381249f63949f2a
Vlad Gafarov <vlad.gafarov02@gmail.com>
2021-07-30 09:57:10 +0500

    Add AnimatePresence

--- frontend/components/LoadingOverlay.tsx
+++ frontend/components/LoadingOverlay.tsx
@@ -2,6 +2,7 @@ import styled, { keyframes } from 'styled-components'
 import tw from 'twin.macro'
 import { CgSpinner } from 'react-icons/cg'
 import { TiTick } from 'react-icons/ti'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const fadeOut = keyframes`
    from {
@@ -12,16 +13,31 @@ const fadeOut = keyframes`
    }
 `
 
-const OverlayStyles = styled.div`
+const OverlayStyles = styled(motion.div)`
    ${tw`
-      pointer-events-none
       absolute
       inset-0
       flex justify-center items-center
+      bg-white bg-opacity-60
       z-10
-      transition
    `}
-   .loader {
+   /* .loader {
+      ${tw`
+         w-16 h-16
+         z-20
+      `}
+   } */
+   .loader, .tick {
+      ${tw`
+         absolute 
+         w-16 h-16
+         top-1/2 left-1/2
+         transform -translate-x-1/2 -translate-y-1/2
+      `}
+   }
+   /* ${props =>
+      props.loading ? tw`bg-white bg-opacity-70 pointer-events-auto` : ''} */
+   /* .loader {
       ${tw`
          hidden
          w-16 h-16
@@ -45,12 +61,17 @@ const OverlayStyles = styled.div`
    .tick {
       ${tw`w-24 h-24`}
       animation: ${props =>
-         !props.error &&
-         !props.loading &&
-         props.called &&
-         fadeOut} 2s cubic-bezier(1,.01,.92,.05);
-   }
+      !props.error &&
+      !props.loading &&
+      props.called &&
+      fadeOut} 2s cubic-bezier(1,.01,.92,.05);
+   } */
 `
+
+const LayersStyles = `w-16 h-16`
+// absolute
+// top-1/2 left-1/2
+// transform -translate-x-1/2 -translate-y-1/2
 
 interface ILoadingOverlay {
    loading: boolean
@@ -60,10 +81,33 @@ interface ILoadingOverlay {
 
 const LoadingOverlay = ({ loading, error, called }: ILoadingOverlay) => {
    return (
-      <OverlayStyles loading={loading} error={error} called={called}>
-         <CgSpinner className="loader animate-spin" />
-         <TiTick fill="#27ae60" className="tick" />
-      </OverlayStyles>
+      // <OverlayStyles loading={loading} error={error} called={called}>
+      //    <CgSpinner className="loader animate-spin" />
+      //    <TiTick fill="#27ae60" className="tick" />
+      // </OverlayStyles>
+      <AnimatePresence exitBeforeEnter>
+         {loading && (
+            <OverlayStyles
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 1 }}
+               exit={{ opacity: 0 }}
+               key="loader"
+            >
+               <CgSpinner className={`${LayersStyles} animate-spin`} />
+            </OverlayStyles>
+         )}
+         {!loading && !error && called && (
+            <OverlayStyles
+               initial={{ opacity: 0 }}
+               animate={{ opacity: [1, 0] }}
+               exit={{ opacity: 0 }}
+               transition={{ duration: 2.5, times: [0.9, 1] }}
+               key="success"
+            >
+               <TiTick fill="#27ae60" className={LayersStyles} />
+            </OverlayStyles>
+         )}
+      </AnimatePresence>
    )
 }
 
