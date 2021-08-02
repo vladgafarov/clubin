174680919f588b489bc01dded6940869e32ba845
Vlad Gafarov <vlad.gafarov02@gmail.com>
2021-08-02 20:27:02 +0500

    Change Loader logic

--- frontend/components/LoadingOverlay.tsx
+++ frontend/components/LoadingOverlay.tsx
@@ -1,111 +1,37 @@
-import styled, { keyframes } from 'styled-components'
-import tw from 'twin.macro'
 import { CgSpinner } from 'react-icons/cg'
-import { TiTick } from 'react-icons/ti'
 import { AnimatePresence, motion } from 'framer-motion'
 
-const fadeOut = keyframes`
-   from {
-      opacity: 1;
-   }
-   to {
-      opacity: 0;
-   }
-`
+const Overlay = ({ children }) => (
+   <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="absolute inset-0 z-10 bg-white-100 bg-opacity-80 rounded flex items-center justify-center"
+   >
+      {children}
+   </motion.div>
+)
 
-const OverlayStyles = styled(motion.div)`
-   ${tw`
-      absolute
-      inset-0
-      flex justify-center items-center
-      bg-white bg-opacity-60
-      z-10
-   `}
-   /* .loader {
-      ${tw`
-         w-16 h-16
-         z-20
-      `}
-   } */
-   .loader, .tick {
-      ${tw`
-         absolute 
-         w-16 h-16
-         top-1/2 left-1/2
-         transform -translate-x-1/2 -translate-y-1/2
-      `}
-   }
-   /* ${props =>
-      props.loading ? tw`bg-white bg-opacity-70 pointer-events-auto` : ''} */
-   /* .loader {
-      ${tw`
-         hidden
-         w-16 h-16
-         transition
-         z-20
-      `}
-   }
-   .loader {
-      ${props => (props.loading ? tw`block` : '')}
-   }
-   .tick {
-      ${tw`
-         absolute 
-         w-16 h-16
-         top-1/2 left-1/2
-         transform -translate-x-1/2 -translate-y-1/2
-         transition
-         opacity-0
-      `}
-   }
-   .tick {
-      ${tw`w-24 h-24`}
-      animation: ${props =>
-      !props.error &&
-      !props.loading &&
-      props.called &&
-      fadeOut} 2s cubic-bezier(1,.01,.92,.05);
-   } */
-`
+const Loading = () => (
+   <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      key="loader"
+   >
+      <CgSpinner size={52} className="animate-spin z-20" />
+   </motion.div>
+)
 
-const LayersStyles = `w-16 h-16`
-// absolute
-// top-1/2 left-1/2
-// transform -translate-x-1/2 -translate-y-1/2
-
-interface ILoadingOverlay {
-   loading: boolean
-   error: boolean
-   called: boolean
-}
-
-const LoadingOverlay = ({ loading, error, called }: ILoadingOverlay) => {
+const LoadingOverlay = ({ loading }: { loading: boolean }) => {
    return (
-      // <OverlayStyles loading={loading} error={error} called={called}>
-      //    <CgSpinner className="loader animate-spin" />
-      //    <TiTick fill="#27ae60" className="tick" />
-      // </OverlayStyles>
-      <AnimatePresence exitBeforeEnter>
+      <AnimatePresence>
          {loading && (
-            <OverlayStyles
-               initial={{ opacity: 0 }}
-               animate={{ opacity: 1 }}
-               exit={{ opacity: 0 }}
-               key="loader"
-            >
-               <CgSpinner className={`${LayersStyles} animate-spin`} />
-            </OverlayStyles>
-         )}
-         {!loading && !error && called && (
-            <OverlayStyles
-               initial={{ opacity: 0 }}
-               animate={{ opacity: [1, 0] }}
-               exit={{ opacity: 0 }}
-               transition={{ duration: 2.5, times: [0.9, 1] }}
-               key="success"
-            >
-               <TiTick fill="#27ae60" className={LayersStyles} />
-            </OverlayStyles>
+            <>
+               <Overlay>
+                  <Loading />
+               </Overlay>
+            </>
          )}
       </AnimatePresence>
    )
