f1dab9238ad2c9d65ecd50e99ee194a53dc08f54
Vlad Gafarov <vlad.gafarov02@gmail.com>
2021-08-03 10:00:48 +0500

    Helpers for notifications

--- frontend/lib/utils/arrayAddRemove.ts
+++ frontend/lib/utils/arrayAddRemove.ts
@@ -1,14 +1,26 @@
-export const remove = (arr: number[], item: number) => {
-   const newArr = [...arr]
+export type Data = {
+   id: number
+   text: string
+}
+
+export const remove = (data: Data[], id: number): Data[] => {
+   const newArr = [...data]
    newArr.splice(
-      newArr.findIndex(i => i === item),
+      data.findIndex(i => i.id === id),
       1
    )
    return newArr
 }
 
-let newIndex = 0
-export const add = (arr: number[]) => {
-   newIndex++
-   return [...arr, newIndex]
+let index: number = 0
+export const add = (data: Data[], text: string): Data[] => {
+   index++
+
+   const newItem = {
+      id: index,
+      text,
+   }
+   const newData = [...data, newItem]
+
+   return newData
 }
