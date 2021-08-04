62e6f21eb4e0c9786a8d2fdfb36ea2511b3f41ad
Vlad Gafarov <vlad.gafarov02@gmail.com>
2021-08-04 09:58:28 +0500

    Remove loading animations

--- frontend/components/Event/ModalButton.tsx
+++ frontend/components/Event/ModalButton.tsx
@@ -1,28 +1,8 @@
 import styled, { keyframes } from 'styled-components'
 import tw from 'twin.macro'
-import { CgSpinner } from 'react-icons/cg'
-import { TiTick } from 'react-icons/ti'
 import { BookEventContext } from './BookEventContext'
 import { useContext } from 'react'
 import { useRegisterModal } from '../../lib/useRegisterModal'
-
-const fade = keyframes`
-  from {
-    opacity: 1;
-  }
-  75% {
-     opacity: 1;
-  }
-  50% {
-     opacity: 1;
-  }
-  25% {
-     opacity: 1;
-  }
-  to {
-    opacity: 0;
-  }
-`
 
 const ButtonStyles = styled.button`
    ${tw`
@@ -37,20 +17,8 @@ const ButtonStyles = styled.button`
       hover:scale-105 
       focus:ring-indigo-500 focus:ring-2
    `}
-   .loading, .success {
-      opacity: 0;
-      pointer-events: none;
-      cursor: wait;
-   }
-   .success {
-      animation: ${props => props.success && fade} 3s 1 ease-out;
-   }
    &:disabled {
       opacity: 0.9;
-      .loading {
-         opacity: 1;
-         pointer-events: all;
-      }
    }
 `
 
@@ -58,7 +26,7 @@ const CancelButtonStyles = styled(ButtonStyles)`
    ${tw`bg-gray-400 focus:ring-gray-300 text-base py-1 px-3`}
 `
 
-const ModalButton: React.FC = () => {
+const ModalButton = () => {
    const {
       handleBookClick,
       handleUnBookClick,
@@ -97,16 +65,9 @@ const ModalButton: React.FC = () => {
             <CancelButtonStyles
                type="button"
                onClick={() => handleUnBookClick(currentEvent.id)}
-               disabled={cancelLoading}
-               success={!cancelLoading && cancelCalled ? true : false}
+               disabled={loading || cancelLoading}
             >
                Cancel book
-               <div className="loading absolute inset-0 flex items-center justify-center text-3xl bg-red-500 ">
-                  <CgSpinner className="animate-spin" />
-               </div>
-               <div className="success absolute inset-0 flex items-center justify-center text-3xl bg-red-500 ">
-                  <TiTick fill="#ffffff" className="tick" />
-               </div>
             </CancelButtonStyles>
          </>
       )
@@ -116,16 +77,9 @@ const ModalButton: React.FC = () => {
       <ButtonStyles
          type="button"
          onClick={() => handleBookClick(currentEvent.id)}
-         disabled={loading}
-         success={!loading && called ? true : false}
+         disabled={loading || cancelLoading}
       >
          Book Event
-         <div className="loading absolute inset-0 flex items-center justify-center text-3xl bg-purple-500 ">
-            <CgSpinner className="animate-spin" />
-         </div>
-         <div className="success absolute inset-0 flex items-center justify-center text-3xl bg-purple-500 ">
-            <TiTick fill="#ffffff" className="tick" />
-         </div>
       </ButtonStyles>
    )
 }
