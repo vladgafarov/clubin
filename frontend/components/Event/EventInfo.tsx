16f90f8bb50b4fd0633702368d3db2e7aa934c0a
Vlad Gafarov <vlad.gafarov02@gmail.com>
2021-08-06 09:46:11 +0500

    Rework of Event info block

--- frontend/components/Event/EventInfo.tsx
+++ frontend/components/Event/EventInfo.tsx
@@ -8,20 +8,18 @@ import { useContext } from 'react'
 import { BookEventContext } from './BookEventContext'
 
 const EventInfoStyles = styled.div`
-   position: relative;
    transition: 0.4s ease-in-out;
-
-   ${tw`col-span-3 mr-12 rounded-xl`}
-   img {
-      object-fit: cover;
+   ${tw`col-span-3 mr-12 rounded-xl overflow-hidden shadow-xl`}
+   &:hover {
+      transform: scale(1.02);
+      img {
+         transform: scale(1.2);
+         transition-duration: 5s;
+      }
    }
    .event-info__text {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
       background-color: rgba(22, 38, 64, 0.6);
-      ${tw`py-4 px-7`}
+      ${tw`py-4 px-7 h-3/5 flex flex-col justify-between`}
    }
    .event-info__text .top {
       ${tw`flex justify-between items-center`}
@@ -39,53 +37,62 @@ const EventInfo = () => {
    }
 
    return (
-      <>
-         <EventInfoStyles className="hidden xl:block">
+      <EventInfoStyles className="hidden xl:block">
+         <div className="relative h-2/5">
             {event?.photo?.image?.publicUrl ? (
                <Image
                   src={event?.photo?.image.publicUrl}
-                  height=""
-                  width=""
-                  sizes="100"
+                  layout="fill"
+                  className="absolute inset-0 object-cover"
                   alt={event?.photo?.altText}
                />
             ) : (
                <Image
                   src="/images/img1.png"
-                  height=""
-                  width=""
-                  sizes="100"
+                  layout="fill"
+                  className="absolute inset-0 object-cover"
                   alt="Best event ever"
                />
             )}
-            <div className="event-info__text">
-               <div className="top">
-                  <h2 className="text-4xl font-pb">{event?.name}</h2>
-                  <p className="flex items-center space-x-2">
-                     <AiOutlineCalendar color="#A919D8" size="25" />
-                     <span className="text-blue-400">
-                        {format(new Date(event?.time), 'dd.LL.yyy')}
-                     </span>
-                  </p>
-               </div>
-               <p className="pt-3 truncate">
-                  {event?.description && event?.description}
+         </div>
+
+         <div className="event-info__text">
+            <div className="top">
+               <h2 className="text-4xl font-pb">{event?.name}</h2>
+               <p className="flex items-center space-x-2">
+                  <AiOutlineCalendar color="#A919D8" size="25" />
+                  <span className="text-blue-400">
+                     {format(new Date(event?.time), 'dd.LL.yyy')}
+                  </span>
                </p>
-               <div className="bottom flex items-center space-x-2 justify-between">
-                  <button
-                     className="underline text-gray-300 hover:text-white"
-                     onClick={openModal}
+            </div>
+            <p className="pt-3 line-clamp-3">
+               {event?.description && event?.description}
+            </p>
+            <div className="flex flex-wrap">
+               {event?.musician.map(item => (
+                  <p
+                     className="bg-purple-800 rounded-full py-1 px-4 text-white mb-2 mr-3"
+                     key={item.id}
                   >
-                     More...
-                  </button>
-                  <div className="flex space-x-2">
-                     <RiMapPinLine color="#A919D8" size="25" />
-                     <span className="text-blue-400">{event?.place}</span>
-                  </div>
+                     {item.name}
+                  </p>
+               ))}
+            </div>
+            <div className="bottom flex items-center space-x-2 justify-between">
+               <button
+                  className="underline text-gray-300 hover:text-white"
+                  onClick={openModal}
+               >
+                  More...
+               </button>
+               <div className="flex space-x-2">
+                  <RiMapPinLine color="#A919D8" size="25" />
+                  <span className="text-blue-400">{event?.place}</span>
                </div>
             </div>
-         </EventInfoStyles>
-      </>
+         </div>
+      </EventInfoStyles>
    )
 }
 
