bf255e2d6a62cc7765c5e5901006b97759570afe
Vlad Gafarov <vlad.gafarov02@gmail.com>
2021-08-09 21:03:54 +0500

    Add favicons

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,8 +4,8 @@ import { ServerStyleSheet } from 'styled-components'
 export default class MyDocument extends Document {
    static getInitialProps({ renderPage }) {
       const sheet = new ServerStyleSheet()
-      const page = renderPage(App => props =>
-         sheet.collectStyles(<App {...props} />)
+      const page = renderPage(
+         App => props => sheet.collectStyles(<App {...props} />)
       )
       const styleTags = sheet.getStyleElement()
       return { ...page, styleTags }
@@ -13,7 +13,33 @@ export default class MyDocument extends Document {
    render() {
       return (
          <Html lang="en">
-            <Head></Head>
+            <Head>
+               <link
+                  rel="apple-touch-icon"
+                  sizes="120x120"
+                  href="/favicons/apple-touch-icon.png"
+               />
+               <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="32x32"
+                  href="/favicons/favicon-32x32.png"
+               />
+               <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="16x16"
+                  href="/favicons/favicon-16x16.png"
+               />
+               <link rel="manifest" href="/favicons/site.webmanifest" />
+               <link
+                  rel="mask-icon"
+                  href="/favicons/safari-pinned-tab.svg"
+                  color="#5bbad5"
+               />
+               <meta name="msapplication-TileColor" content="#da532c" />
+               <meta name="theme-color" content="#ffffff" />
+            </Head>
             <body>
                <Main />
                <NextScript />
