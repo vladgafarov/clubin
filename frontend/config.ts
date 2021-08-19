5c5444090547f857f76be868e1725a8b7fd1f024
Vlad Gafarov <vlad.gafarov02@gmail.com>
2021-08-19 22:02:35 +0500

    Change prod endpoint

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`
-export const prodEndpoint = `https://clubinbackend.herokuapp.com/api/graphql`
+export const prodEndpoint = `http://194.67.91.171:3000/api/graphql`
 export const perPage = 2
